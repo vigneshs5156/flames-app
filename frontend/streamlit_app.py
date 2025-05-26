dcd4bdff1e754dec3b7a7acda8496107d73ff597
vigneshs5156 <vickyvk5156@gmail.com>
2025-05-26 15:59:09 +0530

    made changes in the UI - added a title and spinner for loading

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -3,6 +3,8 @@ import requests
 import json
 
 
+st.title(" Flames \n Know your relation with others")
+
 name_1 = st.text_input("Enter The First Name")
 name_2 = st.text_input("Enter the Second Name")
 
@@ -12,17 +14,18 @@ inputs = {"name_1": name_1, "name_2": name_2}
 data = json.dumps(inputs)
 
 if st.button("Calculate Flames"):
-    try:
-        response = requests.post(
-            url="https://flames-app-nv5g.onrender.com/calculate_flames",
-            data=data,
-            headers={"Content-Type": "application/json"}
-        )
+    with st.spinner("⏳ Waking up backend..."):
+        try:
+            response = requests.post(
+                url="https://flames-app-nv5g.onrender.com/calculate_flames",
+                data=data,
+                headers={"Content-Type": "application/json"}
+            )
 
-        if response.status_code == 200:
-            result = response.text
-            st.subheader(f"{result}")
-        else:
-            st.error(f"Backend Error: {response.status_code}")
-    except Exception as e:
-        st.error(f" Request Failed: {e}")
+            if response.status_code == 200:
+                result = response.text
+                st.success(f"{result}")
+            else:
+                st.error(f"❌ Backend Error: {response.status_code}")
+        except Exception as e:
+            st.error(f"❌ Request Failed: {e}")
