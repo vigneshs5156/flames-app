dbcad2b21567bee7dbf44cf4c11a53f8d379a54b
vigneshs5156 <vickyvk5156@gmail.com>
2025-05-26 13:57:49 +0530

    Initial commit for Streamlit frontend and FastApi backend

---
+++ backend/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class UserInput(BaseModel):
+    name_1 : str
+    name_2 : str
+
+app = FastAPI()
+
+
+
+def calculate_flames(get_name_1, get_name_2):
+    name_1 = [char for char in get_name_1 if char != ' ']
+    name_2 = [char for char in get_name_2 if char != ' ']
+
+    total_length = len(name_1) + len(name_2)
+
+    for word in name_1:
+        if word in name_2:
+            total_length -= 2
+            name_2.remove(word)
+            
+    flames = list('flames')*100
+    lookups = {'f':'are Friends','l':'are in Love','a':'both are having Affection','m':'will end up in Marriage','e':'are Enemies','s':'are Sisters'}
+
+    value = total_length - 1
+
+    for i in range(6):
+        if len(set(flames)) == 1:
+            break
+        redu_list = [i for i in flames if i!= flames[value]]
+        next_word = flames[value+1]
+        flames = redu_list
+        next_index = flames.index(next_word)
+        value = next_index + (total_length - 1)
+        
+    destination = redu_list[0]
+
+    return f"{get_name_1.title()} and {get_name_2.title()} {lookups[destination]}"
+
+
+@app.post("/calculate_flames")
+def calcuate(input: UserInput):
+
+    result = calculate_flames(input.name_1, input.name_2)
+
+    return result
---
+++ frontend/streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+import json
+
+
+name_1 = st.text_input("Enter The First Name")
+name_2 = st.text_input("Enter the Second Name")
+
+
+inputs = {"name_1": name_1, "name_2": name_2}
+
+data = json.dumps(inputs)
+
+if st.button("Calculate Flames"):
+
+    result = requests.post(url="http://127.0.0.1:8000/calculate_flames", data= data)
+
+    st.subheader(result.text)
